0a5e444f5caee3378c3859177acb97bee42ba931
Jinu <jbljw02@naver.com>
2025-02-16 01:10:17 +0900

    fix: appearance:textfield를 통한 숫자 잘림 방지

--- src/components/dixit/scoreboard/ScoreCell.tsx
+++ src/components/dixit/scoreboard/ScoreCell.tsx
@@ -1,4 +1,3 @@
-import CommonInput from '../../common/CommonInput';
 import useScoreCell from '../../../hooks/dixit/useScoreCell';
 import { TABLE_STYLE } from './constant/styles';
 
@@ -34,15 +33,26 @@ export default function ScoreCell({
       {
         editingCell?.playerId === playerId &&
           editingCell?.roundIndex === roundIndex ? (
-          <div className="w-full h-full text-center">
-            <CommonInput
+          <div className="flex items-center justify-center w-full h-full text-center relative">
+            <input
               type="number"
               value={editingScore}
               onChange={cellScoreChange}
               onKeyDown={keyDownEvent}
               onBlur={cellScoreSubmit}
-              errorText={errorMessage}
+              // [appearance:textfield]: input을 일반 텍스트 필드처럼 만들어서 숫자 잘림 방지
+              className="w-[30px] h-[30px] text-center border border-orange-200 
+                rounded-lg focus:outline-none focus:ring-1 focus:ring-orange-400
+                [appearance:textfield]"
               autoFocus={true} />
+            {
+              errorMessage && (
+                <div className="absolute left-1/2 -translate-x-1/2 top-full mt-1
+                  bg-red-200 text-red-600 text-xs px-2 py-1 rounded whitespace-nowrap z-50">
+                  {errorMessage}
+                </div>
+              )
+            }
           </div>
         ) : (
           <div className="w-full h-full flex items-center justify-center">
